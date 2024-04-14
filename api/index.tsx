669503b80cd7650fbb78e2f38ec2989fff160a4b
Spencer <spencerobsitnik@gmail.com>
2024-04-14 12:36:22 -0400

    always serve static

--- api/index.tsx
+++ api/index.tsx
@@ -119,9 +119,9 @@ app.frame("/", (c) => {
 });
 
 // @ts-ignore
-const isEdgeFunction = typeof EdgeFunction !== "undefined";
-const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
-devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
+// const isEdgeFunction = typeof EdgeFunction !== "undefined";
+// const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, { serveStatic });
 
 export const GET = handle(app);
 export const POST = handle(app);
